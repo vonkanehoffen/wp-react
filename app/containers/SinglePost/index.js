5f0cdb1f01cb566958a43978a3b05d46de4558c1
Kane Clover <kane@assure24.com>
2017-04-04 22:25:45 +0100

    fire featured media action

--- app/containers/SinglePost/index.js
+++ app/containers/SinglePost/index.js
@@ -10,21 +10,51 @@ import Helmet from 'react-helmet';
 import config from 'config';
 import Post from 'components/Post';
 import { find } from 'lodash';
-import { loadPosts } from '../../store/posts/actions';
+import { loadPosts, loadFeaturedMedia } from '../../store/posts/actions';
 import ActionBar from 'containers/ActionBar';
+import FeaturedMedia from 'components/FeaturedMedia';
+import styled from 'styled-components';
 
 export class SinglePost extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
-  componentDidMount() {
-    const { posts, params, dispatch } = this.props;
-    if(!posts.find(post => post.slug === params.slug)) {
-      dispatch(loadPosts({slug: params.slug}));
+  findPost(slug) {
+    return this.props.posts.find(post => post.slug === slug)
+  }
+
+  checkFeaturedMedia() {
+    const { params, dispatch } = this.props
+    const post = this.findPost(params.slug);
+    if(post && post.featured_media > 0) {
+      dispatch(loadFeaturedMedia(post.id))
     }
   }
+
+  // If we don't already have the post stored, load it
+
+  componentDidMount() {
+    const { params, dispatch } = this.props
+    if(!this.findPost(params.slug)) {
+      dispatch(loadPosts({slug: params.slug}))
+    } else {
+      this.checkFeaturedMedia()
+    }
+  }
+
+  // If the post is loaded and it should have a featured image, load that too
+
+  componentDidUpdate(prevProps) {
+    this.checkFeaturedMedia()
+  }
+
   render() {
     const { posts, params } = this.props;
-    const post = posts.find(post => post.slug === params.slug);
+    const post = this.findPost(params.slug);
     const title = post ? post.title.rendered : '';
+    const featuredMedia = post ? post.featured_media : 0;
+
+    const Spacer = styled.div`
+      height: 60px;
+    `;
 
     return (
       <div>
@@ -34,7 +64,11 @@ export class SinglePost extends React.Component { // eslint-disable-line react/p
             { name: 'description', content: title },
           ]}
         />
-        <h1>SinglePost: {params.slug}</h1>
+        {post && post.featured_media > 0 ?
+          <FeaturedMedia post={post} />
+        :
+          <Spacer />
+        }
         {post && <Post post={post}/>}
         <ActionBar />
       </div>
