edb5d35b3d79845616d01d0ac4e8fe62124161c4
Kane Clover <kane@assure24.com>
2017-04-17 07:33:26 +0100

    Link tag

--- app/components/Tags/index.js
+++ app/components/Tags/index.js
@@ -13,7 +13,7 @@ function Tags({ terms }) {
   return (
     <ul>
       {terms.map(term =>
-        <li>Tag: <Link to={`/tag/slug/${term.slug}/`}>{term.name}</Link></li>
+        <li>Tag: <Link to={`/tag/${term.slug}/`}>{term.name}</Link></li>
       )}
     </ul>
   );
