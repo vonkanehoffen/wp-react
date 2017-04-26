6637d78f2791aff83e4fc3adc2b741d575d19f85
Kane Clover <kane@assure24.com>
2017-04-26 22:26:06 +0100

    Add container

--- app/components/HomeSplash/index.js
+++ app/components/HomeSplash/index.js
@@ -12,8 +12,10 @@ function HomeSplash() {
   return (
     <section id="HomeSplash">
       <div className="inner">
-        <h1><span>Kane Clover</span> / Manchester, UK</h1>
-        <h2>UX Designer &amp; Full Stack Developer</h2>
+        <div className="container">
+          <h1><span>Kane Clover</span> / Manchester, UK</h1>
+          <h2>UX Designer &amp; Full Stack Developer</h2>
+        </div>
       </div>
     </section>
   );
