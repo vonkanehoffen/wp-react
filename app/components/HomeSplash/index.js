f55dbe03ba5b94c4dae6046e6f185cf05aa9975b
Kane Clover <kane@assure24.com>
2017-05-16 20:05:17 +0100

    add existing video splash

--- app/components/HomeSplash/index.js
+++ app/components/HomeSplash/index.js
@@ -11,6 +11,12 @@ import './style.scss'
 function HomeSplash() {
   return (
     <section id="HomeSplash">
+      <div className="videoBg">
+        <video autoPlay loop poster="http://kanec.co.uk/wp-content/themes/kanec/assets/images/splash.jpg" id="splash-video-bg">
+          <source src="http://kanec.co.uk/wp-content/themes/kanec/assets/images/splash.webm" type="video/webm" />
+          <source src="http://kanec.co.uk/wp-content/themes/kanec/assets/images/splash.mp4" type="video/mp4" />
+        </video>
+      </div>
       <div className="inner">
         <div className="container">
           <h1><span>Kane Clover</span> / Manchester, UK</h1>
