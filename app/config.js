e5c85f88833c10a84df049439b4b5f6ae92df452
Kane Clover <kane@assure24.com>
2017-12-29 15:41:51 +0000

    base WP theme

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 export default {
   blogTitle: 'Kanec Blog',
-  apiRoot: 'https://kanec.badgerweb.net/wp-json/wp/v2',
-  staticAssetRoot: 'https://kanec.uk/static_assets',
+  apiRoot: 'https://kanec.uk/wp-json/wp/v2',
+  staticAssetRoot: 'https://kanec.uk/wp-content/themes/wp-react/static_assets',
   maxWidth: '1170px',
   primaryColor: '#F44336',
   primaryColorLight: '#D32F2F',
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Main WP Template.
+ * Just prints the base to load react. See generated index.html in build/
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="manifest" href="manifest.json">
+    <meta name="mobile-web-app-capable" content="yes">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400italic,700,700italic" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,500" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Kane Clover - UX Designer & Full Stack Developer</title></head>
+<body>
+<noscript>If you're seeing this message, that means <strong>JavaScript has been disabled on your browser</strong>,
+    please <strong>enable JS</strong> to make this app work.
+</noscript>
+<div id="root"></div>
+<script type="text/javascript" src="/build/main.3bbf9710fdebf2d19657.js"></script>
+</body>
+</html>
